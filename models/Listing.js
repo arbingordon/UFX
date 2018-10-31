7c0b5b77e986cd5e28aeef40fb18426229e63b07
John Kearney <arbingordon@gmail.com>
2018-10-31 14:24:42 -0400

    switch to passport, base site kinda working

---
+++ models/Listing.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// var passportLocalMongoose = require('passport-local-mongoose');
+
+var ListingSchema = new Schema({
+    shortdesc: String,
+    longdesc: String,
+    tags:     String[],
+    price: Number,
+    address: String
+});
+
+//ListingSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Listing', ListingSchema);
